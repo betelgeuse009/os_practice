224076b4a25a11e612bcf4edac93160722bf2780
Erdeniz Esmeli <erdenizesmeli@proton.me>
2025-12-23 16:18:49 +0100

    Solved ex08

--- scripts/ex08-exam.sh
+++ scripts/ex08-exam.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
-# Given a file named "file.txt" containing some words (i.e., sequences of alphabetic characters separated by
-# spaces, even on multiple lines) with content similar to the following:
-# one two three
-# four five six
-# seven
-# Implement a bash script that prints the word with longer length.
-# Given the previous example, the script must print in output the word "three" or the word "seven", because both
-# have 5 characters
+# Write a bash script able to compute the sum of lengths of the words appearing on the diagonal of a square
+# matrix that contains just strings.
+# The square matrix is stored in a text file, specified as a command line argument to the script. Check that such
+# an argument is correctly passed to the script.
+# Example
+# word11 word12 word13 word14
+# word21 word22 word23 word24
+# word31 word32 word33 word34
+# word41 word42 word43 word44
+# Result: 24
 
-fn="file.txt"
-max=$((-1))
-longest_words=""
-for word in $(cat "$fn"); do
-  wsize=$(echo -n "$word" | wc -c)
-  if [ "$wsize" -gt "$max" ]; then
-    max=$((wsize))
-    longest_words="$word"
-  elif [ "$wsize" -eq "$max" ]; then
-    longest_words="$longest_words $word"
-  fi
-done
-echo "$longest_words"
+fn=$1
+
+if [ $# -ne 2 ]; then
+  echo "ERROR wrong number of arguments run as $0 <file_name>"
+  exit 1
+fi
+
+if [ ! -f "$fn" ]; then
+  echo "ERROR run as $0 <file_name>"
+  exit 1
+fi
+
+i=0
+j=0
+tot=0
+while read line; do
+  for word in $line; do
+    if [ $i -eq $j ]; then
+      wsize=$(echo -n "$word" | wc -c)
+      tot=$((tot + wsize))
+    fi
+    j=$((j++))
+  done
+  j=$((0))
+  i=$((i++))
+done <"$fn"
