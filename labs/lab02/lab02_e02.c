c645acfff52d540776810cf55805b175548ff6d0
Erdeniz Esmeli <erdenizesmeli@proton.me>
2025-12-29 15:27:55 +0100

    modified lab02 e02.c

--- labs/lab02/lab02_e02.c
+++ labs/lab02/lab02_e02.c
@@ -1,13 +1,12 @@
-// A program receives two integer values ​​on the command line, 
-// called n and t. The program (parent process) must produce 2 children and terminate.
-// In turn, each child must produce 2 children and terminate. 
-// This sequence of operations must continue until 2 ^ n processes 
-// on the leaves of the tree are produced/running. The leaf processes 
-// wait for t seconds and display (on screen) a termination message.
-// Note that each process (in the tree) produces two other processes.
-// Only those on the leaves of the tree sleep and display a message. 
-// What is the order of termination of the processes? Is it always the same? 
-// How can they be recognized (ppid)?
+// A program receives two integer values ​​on the command line,
+// called n and t. The program (parent process) must produce 2 children and
+// terminate. In turn, each child must produce 2 children and terminate. This
+// sequence of operations must continue until 2 ^ n processes on the leaves of
+// the tree are produced/running. The leaf processes wait for t seconds and
+// display (on screen) a termination message. Note that each process (in the
+// tree) produces two other processes. Only those on the leaves of the tree
+// sleep and display a message. What is the order of termination of the
+// processes? Is it always the same? How can they be recognized (ppid)?
 //
 // Once the program has been executed:
 //
@@ -21,22 +20,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
-int main(int argc, int *argv[]){
-  int counter=0;
+int main(int argc, char *argv[]) {
+  int counter = 0;
   int n = atoi(argv[1]);
   int t = atoi(argv[2]);
   pid_t pid1;
   pid_t pid2;
 
+  fprintf(stdout, "PID=%d; PPID=%d; counter=%d", getpid(), getppid(), counter);
 
-  for (int i=0; i<n; i++ ){
+  for (int i = 0; i < n; i++) {
     pid1 = fork();
-    
-    if (pid1==0){
 
+    if (pid1 == 0) {
+      // child1
+      fprintf(stdout, "PID=%d; PPID=%d; counter=%d", getpid(), getppid(),
+              counter);
     }
-
   }
   return 0;
 }
